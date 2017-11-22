259d535457d54dd233add268c2aeb482ef3b0eff
stiven1321 <strincon@poligran.edu.co>
2017-11-21 23:43:06 -0500

    2

--- Music-Console/src/console/Controller.java
+++ Music-Console/src/console/Controller.java
@@ -3,14 +3,40 @@ package console;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.text.Text;
 
 public class Controller {
 	
-	@FXML private Text actiontarget;
+	
     
-    @FXML protected void gonorrea(ActionEvent event) {
-       System.out.println("re gonorrea....");
+    @FXML protected void Accion1(ActionEvent event) {
+       System.out.println("Parar....");
        
+    }
+    
+    @FXML protected void Accion2(ActionEvent event) {
+        System.out.println("Empesar....");
+        
+     }
+    
+    @FXML protected void EntradaEncendida(ActionEvent event){
+    	
+    	Alert r = new Alert(AlertType.INFORMATION);
+    	r.setTitle("Entrada");
+    	r.setHeaderText("La entrada aesta abierta");
+    	r.setContentText("Puedes empezar a hablar");
+    	r.showAndWait(); 
+    }
+    
+@FXML protected void SalidaEncendida(ActionEvent event){
+    	
+    	Alert r = new Alert(AlertType.INFORMATION);
+    	r.setTitle("Salida");
+    	r.setHeaderText("La Salida esta encendida");
+    	r.setContentText("Puedes empezar a escuchar");
+    	r.showAndWait(); 
     }
 }
